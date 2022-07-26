96108c08453bc48e6d4670994ebf6e0b065608c1
CaioVRS21 <veras.caio12@gmail.com>
2022-07-26 19:28:34 -0400

    Projeto finalizado

---
+++ main.js
@@ -0,0 +1,75 @@
+function criaCalculadora(){
+    return{
+        display: document.querySelector('.display'),
+
+        inicia(){
+            this.cliqueBtn();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13){
+                    this.realizaConta();
+                }
+            })
+        },
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        btnBackspace(){
+            this.display.value = this.display.value.slice(0, -1)
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta Inválida');
+                    return;
+                }
+
+                this.display.value = conta;
+            } catch(e){
+                alert('Conta Inválida');
+                    return;
+            }
+        },
+
+        cliqueBtn(){
+            //                                arrow functions travam o .this no .this principal (nesse caso seria a variavel calculadora ali embaixo)
+            document.addEventListener('click', e => {
+                const el = e.target;
+                console.log(this)
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')){
+                    this.btnBackspace();
+                }
+
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        },
+
+         
+    };
+};
+const calculadora = criaCalculadora();
+calculadora.inicia();
